755ab739b895f612d007998afabb175022397591
nabacg <nabacg@gmail.com>
2016-04-24 19:50:21 +0100

    optimized solution

--- eight_queens.pl
+++ eight_queens.pl
@@ -2,20 +2,15 @@
 eight_queens(Board) :-
     Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
     valid_board(Board),
-    rows(Board, Rows), all_distinct(Rows),
     cols(Board, Cols), all_distinct(Cols),
-    diags1(Board, ds1), all_distinct(ds1),
-    diags2(Board, ds2), all_distinct(ds2).
+    diags1(Board, Ds1), all_distinct(Ds1),
+    diags2(Board, Ds2), all_distinct(Ds2).
 
 valid_queen((_, Column)) :-
     member(Column, [1,2,3,4,5,6,7,8]).
 
 valid_board([]).
 valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
-
-rows([], []).
-rows([(Row, _)|QueensTail], [Row|RowsTail]) :- 
-    rows(QueensTail, RowsTail).
 
 cols([], []).
 cols([(_, Col)|QueensTail], [Col|ColsTail]) :- 
